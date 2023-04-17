9865c39e95546fb8c3f6c2f1d81f180c88dafd95
RRRMartins <ribeiro.roulien@gmail.com>
2023-04-17 10:34:19 -0300

    adicionando_app

--- Projetos/calculadora/__init__.py
+++ Projetos/calculadora/__init__.py
@@ -1,24 +1,25 @@
-from funcoes import soma
-from funcoes import subtracao
-from funcoes import multiplicacao
-from funcoes import divisao
+from Projetos.calculadora.funcoes import soma
+from Projetos.calculadora.funcoes import subtracao
+from Projetos.calculadora.funcoes import multiplicacao
+from Projetos.calculadora.funcoes import divisao
 
-a = float(input('Insira o primeiro valor: '))
-b = float(input('Insira o segundo valor: '))
+def calcule():
+    a = float(input('Insira o primeiro valor: '))
+    b = float(input('Insira o segundo valor: '))
 
-operacao = input('Informe a operação desejada (soma, subtracao, divisao, multiplicacao, +, -, /, *): ').lower()
+    operacao = input('Informe a operação desejada (soma, subtracao, divisao, multiplicacao, +, -, /, *): ').lower()
 
-if operacao == 'soma' or operacao == '+':
-    print(f'O resultado da sua operação de soma: {soma(a, b)}')
+    if operacao == 'soma' or operacao == '+':
+        print(f'O resultado da sua operação de soma: {soma(a, b)}')
 
-elif operacao == 'subtracao' or operacao == '-':
-    print(f'O resultado da sua subtração:  {subtracao(a, b)}')
+    elif operacao == 'subtracao' or operacao == '-':
+        print(f'O resultado da sua subtração:  {subtracao(a, b)}')
 
-elif operacao == 'multiplicacao' or operacao == '*':
-    print(f'O resultado da sua multiplicação: {multiplicacao(a, b)}')
+    elif operacao == 'multiplicacao' or operacao == '*':
+        print(f'O resultado da sua multiplicação: {multiplicacao(a, b)}')
 
-elif operacao == 'divisao' or operacao == '/':
-    print(f'O resultado da sua divisão: {divisao(a, b)}')
+    elif operacao == 'divisao' or operacao == '/':
+        print(f'O resultado da sua divisão: {divisao(a, b)}')
 
-else:
-    print(f'Operação desejada inválida!')
+    else:
+        print('Operação desejada inválida!')
